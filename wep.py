4000eea965fb31429cc716d137d1e81e5d1cf71b
Rajnish Kumar <raj.kr01001@gmail.com>
2025-03-27 16:43:32 +0530

    Update wep.py

--- wep.py
+++ wep.py
@@ -1,46 +1,29 @@
-import tkinter as tk
-from tkinter import messagebox
+import streamlit as st
+
 import requests
 
-API_KEY = 'cc1c8c16dc414858a9e90359250303'
-BASE_URL = 'http://api.weatherapi.com/v1/current.json'
+api_url = "http://api.weatherapi.com/v1/current.json?"
+api_key = "0c969b83b3324359b5685710250303"
 
-def get_weather(city):
-    params = {
-        'key': API_KEY,
-        'q': city
-    }
-    response = requests.get(BASE_URL, params=params)
-    return response.json()
+st.title("Weather App")
 
-def show_weather():
-    city = city_entry.get()
-    weather = get_weather(city)
-    
-    if 'error' in weather:
-        messagebox.showerror("Error", weather['error']['message'])
-        return
-    
-    temperature = weather['current']['temp_c']
-    description = weather['current']['condition']['text']
-    weather_label.config(text=f"{city} \n Temperature: {temperature}°C\nDescription: {description}")
+city = st.text_input("Enter a city name:")
 
+if city:
+    url = api_url + "key=" + api_key + "&q=" + city
+    res = requests.get(url)
+    if res.status_code == 200:
+        data = res.json()
+        st.write(f"The current Temperature in {city} is : {data['current']['temp_c']} °C")
+        st.write(data['current'])
+    else:
+        st.write("City not found")
 
-root = tk.Tk()
-root.title("Weather App")
-
-
-city_label = tk.Label(root, text="Enter city name:")
-city_label.pack(pady=5)
-
-city_entry = tk.Entry(root)
-city_entry.pack(pady=5)
-
-get_weather_button = tk.Button(root, text="Get Weather", command=show_weather)
-get_weather_button.pack(pady=10)
-
-weather_label = tk.Label(root, text="", font=("Helvetica", 14))
-weather_label.pack(pady=20)
-
-
-root.mainloop()
+hide_menu = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+header {visibility: hidden;}
+</style>
+"""
+st.markdown(hide_menu, unsafe_allow_html=True)
